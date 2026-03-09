eac25746fa2c6962a8e29963780270bb0712b723
cheami <cheami90@gmail.com>
2026-03-09 01:05:39 +0000

    remove unnecessary comments

--- travel_bundle/src/travel_data_etl/transformations/silver_manual_log.sql
+++ travel_bundle/src/travel_data_etl/transformations/silver_manual_log.sql
@@ -1,6 +1,4 @@
--- Define a materialized view that validates data and renames a column
 CREATE OR REFRESH MATERIALIZED VIEW silver.manual_log(
---CONSTRAINT valid_artist_name EXPECT (artist_name IS NOT NULL)
 )
 COMMENT "Manual log containing travel itinerary for each day"
 AS SELECT Day,Date,Country,City,Description,Comments,Food,Travel,Hotel
